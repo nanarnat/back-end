5c1c45908fc51bb53c5645f9b414007a251c1100
Nattha <gamenattha555@gmail.com>
2025-12-29 14:08:55 +0700

    add cors

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const corsOptions = {
     origin: [
         "http://localhost:5173",
         "http://localhost:5174",
-        "http://localhost:5175",
+        "https://front-end-lovat-ten.vercel.app/",
     ],
 }
 
